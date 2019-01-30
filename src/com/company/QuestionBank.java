b0a6d1706bb04e76e74e4482ff6f1b8fb442ea5d
Brian B <brianb@MacBook-Pro-4.local>
2019-01-30 13:46:15 -0500

    Adds createQuestionSet method to QuestionBank returning a set of 5 random questions

--- src/com/company/QuestionBank.java
+++ src/com/company/QuestionBank.java
@@ -2,14 +2,34 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class QuestionBank {
 
-    List<QuizQuestion> questionBank = new ArrayList<>();
+    Random random = new Random();
+
+    List<QuizQuestion> questionBank;
+
+    public QuestionBank(){
+        questionBank = new ArrayList<>();
+    }
 
 
     public void addQuestion(QuizQuestion question){
         questionBank.add(question);
+    }
+
+    public ArrayList<QuizQuestion> createQuestionSet(){
+
+        ArrayList<QuizQuestion> questionSet = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++ ){
+         QuizQuestion question = questionBank.get(random.nextInt(questionBank.size() + 1));
+         questionSet.add(question);
+        }
+
+
+        return questionSet;
     }
 
 }
---
+++ src/com/company/Quiz.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+
+    private List<QuizQuestion> quizQuestionSet;
+    private ScoreKeeper scoreKeeper;
+    private QuestionBank questionBank;
+
+    public Quiz(){
+        this.questionBank = new QuestionBank();
+       ArrayList<QuizQuestion> questionSet = this.questionBank.createQuestionSet();
+
+        this.quizQuestionSet = new ArrayList(questionSet);
+        this.scoreKeeper = new ScoreKeeper();
+    }
+}
